98c6fb888e1caabc1e0678864bb675082d064f27
Kilian-Christen <71339610+Kilian-Christen@users.noreply.github.com>
2021-05-27 16:57:31 +0200

    Maybe fixed the bug

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Drawing.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Drawing.java
@@ -25,7 +25,7 @@ public class Drawing implements Serializable {
 
 
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @Fetch(value = FetchMode.SUBSELECT)
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private List<BrushStroke> brushStrokes = new ArrayList<>();
--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/DrawingService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/DrawingService.java
@@ -134,18 +134,31 @@ public class DrawingService {
     public void addStroke(Drawing drawing, BrushStroke brushStroke) {
         brushStroke = brushStrokeRepository.save(brushStroke); // save in repository
         brushStrokeRepository.flush();
+        System.out.println(brushStroke.getId());
 
-        drawing.getBrushStrokes().add(brushStroke); // add to drawing
-        // for safety we check if it works
+            drawing.getBrushStrokes().add(brushStroke); // add to drawing
+
         try {
             Collections.sort(drawing.getBrushStrokes()); // sort list within drawing
         } catch (ClassCastException e) {
             System.out.println("You tried to sort a list with objects that can not be compared with one another.");
         }
+       // try {
+       // drawingRepository.save(drawing);
+       // } catch (Exception e){
+       //     e.printStackTrace();
+       //
+       // }
+       // drawingRepository.flush();// update the drawing in the repository
+       //
+       //
+        }
 
-        drawingRepository.saveAndFlush(drawing); // update the drawing in the repository
+    public void save(Drawing drawing){
+        drawingRepository.save(drawing);
+        drawingRepository.flush();
+
     }
-
     // get all rounds
     public List<Round> getRounds() {
         return this.roundRepository.findAll();
