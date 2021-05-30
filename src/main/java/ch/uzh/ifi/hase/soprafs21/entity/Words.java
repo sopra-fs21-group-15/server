1476271b3ff0d6039a1ac9ece0eeafa2ac76f730
niklassc <niklas@schmatloch.de>
2021-05-30 20:00:52 +0200

    syntax

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Words.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Words.java
@@ -5,10 +5,10 @@ import java.util.Random;
 
 public class Words {
 
-    private final String[] words = { "house", "dog", "fish", "spine", "dream,
+    private final String[] words = { "house", "dog", "fish", "spine", "dream",
             "murder", "study", "bean", "ice cream", "train", "calculator",
             "bench", "drill", "laptop", "apple", "lake", "ocean", "ship",
-            "compass", "physics", "bar", "painting", "cinema", "lighthouse"
+            "compass", "physics", "bar", "painting", "cinema", "lighthouse",
             "parliament", "earthquake", "volcano", "curve", "relationship",
             "fraction", "maths", "piano", "guitar", "harmonica", "letter",
             "jewel", "addiction", "strawberry", "orbit", "onion",
