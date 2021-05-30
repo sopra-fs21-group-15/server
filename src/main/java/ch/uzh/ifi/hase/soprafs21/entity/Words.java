aeda9e97e6a34ed7bf7f8a201f527ee8b9aee389
niklassc <niklas@schmatloch.de>
2021-05-30 19:59:29 +0200

    add some more words

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Words.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Words.java
@@ -5,11 +5,15 @@ import java.util.Random;
 
 public class Words {
 
-    private final String[] words = { "house", "dog", "fish", "spine",
-            "murder", "study", "bean", "ice cream", "train", "calculator", "bench", "drill",
-            "compass", "physics", "bar", "painting", "cinema",
-            "parliament", "earthquake", "volcano", "curve", "relationship", "fraction",
-            "jewel", "addiction", "strawberry", "orbit", "onion", "influencer", "twilight", "horse", "summer", "ghost",
+    private final String[] words = { "house", "dog", "fish", "spine", "dream,
+            "murder", "study", "bean", "ice cream", "train", "calculator",
+            "bench", "drill", "laptop", "apple", "lake", "ocean", "ship",
+            "compass", "physics", "bar", "painting", "cinema", "lighthouse"
+            "parliament", "earthquake", "volcano", "curve", "relationship",
+            "fraction", "maths", "piano", "guitar", "harmonica", "letter",
+            "jewel", "addiction", "strawberry", "orbit", "onion",
+            "influencer", "twilight", "horse", "summer", "ghost", "camera",
+            "bottle", "banana", "lightbulb", "circus", "zoo", "table", "arm",
             "switzerland", "ellipse", "rib", "scotland", "color-blind" };
 
     public String getRandomWord() {
